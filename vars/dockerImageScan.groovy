b3b13b0a1770a120afc4696e14afbfef434b4961
Diawara <32206533+sdiawa@users.noreply.github.com>
2024-02-04 17:11:32 +0100

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -4,12 +4,11 @@
 //      trivy image ${hubUser}/${project}:latest > scan.txt
 //      cat scan.txt
 //     """
-// }
+// }    
+def call (){
 
-def call(String aws_account_id, String region, String ecr_repoName){
-    
     sh """
-    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+    trivy ${params.ImageName}:${params.ImageTag} ${params.DockerHubUser}/${params.ImageName}:${params.ImageTag} > scan.txt
     cat scan.txt
     """
 }
