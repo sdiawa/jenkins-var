98edf0714ec2b51bbae797fdb694f4341fc120f3
Diawara <32206533+sdiawa@users.noreply.github.com>
2024-02-04 18:58:41 +0100

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,15 +1,8 @@
-// def call(String project, String ImageTag, String hubUser){
+def call(){
     
-//     sh """
-//      docker rmi ${hubUser}/${project}:${ImageTag}
-//      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
-//     """
-// }
+  sh """
+  docker rmi ${params.ImageName}:${params.ImageTag}
+  docker rmi ${params.DockerHubUser}/${params.ImageName}:${params.ImageTag}
 
-
-def call(String aws_account_id, String region, String ecr_repoName){
-    
-    sh """
-     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+"""
 }
