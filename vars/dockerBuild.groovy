d1c16ff6b7317ec95a3a86d6d1dc5f5eb08de308
Diawara <32206533+sdiawa@users.noreply.github.com>
2024-02-04 14:25:09 +0100

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,17 +1,8 @@
-// def call(String project, String ImageTag, String hubUser){
-    
-//     sh """
-//      docker image build -t ${hubUser}/${project} . 
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-//     """
-// }
-
-def call(String ecr_repoName){
+def call(String project, String ImageTag, String hubUser){
     
     sh """
-
-     docker.build -t ${ecr_repoName} .
-     docker tag ${ecr_repoName}:latest 
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
-}
+ }
