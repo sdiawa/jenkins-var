e65816b6f2fee492d528866fc2e1db1bf8d4b1f2
Diawara <32206533+sdiawa@users.noreply.github.com>
2024-02-04 16:40:39 +0100

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,17 @@
-def call(String project, String ImageTag, String hubUser){
+//def call(String project, String ImageTag, String hubUser){
     
-    sh """
-     docker image build -t ${hubUser}/${project} . 
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-    """
- }
+def customImage = docker.build("${params.ImageName}:${params.ImageTag}")
+                    
+// Utilisez les credentials Docker Hub
+  withCredentials([usernamePassword(credentialsId: ${params.DockerHubCredentialsId}, usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
+    // Connexion à Docker Hub
+   sh "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"                        
+    // Ajoutez la commande docker tag
+   sh "docker tag ${params.ImageName}:${params.ImageTag} ${params.DockerHubUser}/${params.ImageName}:${params.ImageTag}"
+     // Poussez l'image avec le tag spécifié
+   sh "docker push ${params.DockerHubUser}/${params.ImageName}:${params.ImageTag}"
+          }
+  // Exécutez des commandes à l'intérieur du conteneur Docker (optionnel)
+    ustomImage.inside {
+                        // ...
+  }
